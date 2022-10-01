7b1b4aca91c1801a93549dc3c6e621b52aec9d74
kishanjvns <kishanjvns@hotmail.com>
2022-10-01 20:44:52 +0530

    actuator change

--- src/main/java/com/tech/kj/SpringBootTestBasicApplication.java
+++ src/main/java/com/tech/kj/SpringBootTestBasicApplication.java
@@ -2,6 +2,9 @@ package com.tech.kj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 public class SpringBootTestBasicApplication {
